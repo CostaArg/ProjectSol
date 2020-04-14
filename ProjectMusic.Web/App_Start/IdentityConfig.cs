cc33922b1be5d4cef9c66e1d7dbf81c6527abfad
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-04-14 17:26:57 +0300

    added login functionality

--- ProjectMusic.Web/App_Start/IdentityConfig.cs
+++ ProjectMusic.Web/App_Start/IdentityConfig.cs
@@ -54,10 +54,10 @@ namespace ProjectMusic.Web
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
