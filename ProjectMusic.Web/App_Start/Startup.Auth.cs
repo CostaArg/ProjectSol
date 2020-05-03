3a11ae6a64647067ed8076a8a88fa7ef95cddcd1
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-05-03 18:21:28 +0300

    updated microsoft login

--- ProjectMusic.Web/App_Start/Startup.Auth.cs
+++ ProjectMusic.Web/App_Start/Startup.Auth.cs
@@ -8,12 +8,31 @@ using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.Twitter;
 using Owin;
 using ProjectMusic.Web.Models;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.Notifications;
+using System.Threading.Tasks;
 
 namespace ProjectMusic.Web
 {
     public partial class Startup
     {
-        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
+
+        // The Client ID is used by the application to uniquely identify itself to Microsoft identity platform.
+        string clientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
+
+        // RedirectUri is the URL where the user will be redirected to after they sign in.
+        string redirectUri = System.Configuration.ConfigurationManager.AppSettings["RedirectUri"];
+
+        // Tenant is the tenant ID (e.g. contoso.onmicrosoft.com, or 'common' for multi-tenant)
+        static string tenant = System.Configuration.ConfigurationManager.AppSettings["Tenant"];
+
+        // Authority is the URL for authority, composed by Microsoft identity platform endpoint and the tenant name (e.g. https://login.microsoftonline.com/contoso.onmicrosoft.com/v2.0)
+        string authority = String.Format(System.Globalization.CultureInfo.InvariantCulture, System.Configuration.ConfigurationManager.AppSettings["Authority"], tenant);
+
+
+
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
@@ -62,11 +81,39 @@ namespace ProjectMusic.Web
             //})
             //        });
 
-            //Uncomment the following lines to enable logging in with third party login providers
+            #region Microsoft Authentication
 
-            app.UseMicrosoftAccountAuthentication(
-                clientId: "1946b896-19c5-4720-957b-d6069d890383",
-                clientSecret: "=/2nl1F3O-7LUBt?s7xD[e9Q0g6s=R_z");
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                // Sets the ClientId, authority, RedirectUri as obtained from web.config
+                ClientId = clientId,
+                    Authority = authority,
+                    RedirectUri = redirectUri,
+                // PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it is using the home page
+                PostLogoutRedirectUri = redirectUri,
+                    Scope = OpenIdConnectScope.OpenIdProfile,
+                // ResponseType is set to request the id_token - which contains basic information about the signed-in user
+                ResponseType = OpenIdConnectResponseType.IdToken,
+                // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
+                // To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name
+                // To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter
+                TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false // This is a simplification
+                },
+                // OpenIdConnectAuthenticationNotifications configures OWIN to send notification of failed authentications to OnAuthenticationFailed method
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = OnAuthenticationFailed
+                    }
+                }
+            );
+
+            #endregion
 
             app.UseTwitterAuthentication(
             consumerKey: "vpjXLGkLYfkjNEYVEo1WETePc",
@@ -82,5 +129,15 @@ namespace ProjectMusic.Web
                 ClientSecret = "FHZxc_Sgbcr1YYcsTQjlyreE"
             });
         }
+
+        //Microsoft Failed Authentication
+
+        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> context)
+        {
+            context.HandleResponse();
+            context.Response.Redirect("/?errormessage=" + context.Exception.Message);
+            return Task.FromResult(0);
+        }
+
     }
 }
