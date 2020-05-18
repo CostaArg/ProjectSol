ab754e62a37d55b5746ee504f716e01852f30b7c
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-05-18 16:07:54 +0300

    refactored navbar

---
+++ ProjectMusic.Web/Areas/User/Controllers/UserOrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMusic.Web.Areas.User.Controllers
+{
+    public class UserOrderController : Controller
+    {
+        // GET: User/UserOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
